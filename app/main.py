936b186217187c6d9a93218e92044f31a9a97a19
RajashekarRM1 <rajashekar.maddi@rm1codershub.com>
2025-12-04 15:15:20 +0530

    pushed the code with email notification

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from routes.auth import router as user_router
 from dotenv import load_dotenv
 import os
 from routes.freelancer_route import router as freelancer_router
-from routes.service_route import router as service_router
+# from routes.service_route import router as service_router
 
 
 load_dotenv()
@@ -12,7 +12,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(user_router)
 app.include_router(freelancer_router)
-app.include_router(service_router)
+# app.include_router(service_router)
 
 
 
