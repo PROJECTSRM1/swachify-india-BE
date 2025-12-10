22e3f57d2aaf188506527c015fcfe8605b920dd2
RajashekarRM1 <rajashekar.maddi@rm1codershub.com>
2025-12-10 12:58:36 +0530

    Update database.py

--- core/database.py
+++ core/database.py
@@ -21,9 +21,11 @@ DATABASE_URL = f"postgresql://{USER_ENC}:{PASSWORD_ENC}@{DB_HOST}:{DB_PORT}/{DB_
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10,
+    connect_args={"connect_timeout": 5},
     future=True,
 )
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
