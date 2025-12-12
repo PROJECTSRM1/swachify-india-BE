5d70fa8cd2d7464c60164c0fb07606c51ea84bfc
LOKESWARARM1 <lokeswara.mayaluri@rm1codershub.com>
2025-12-12 09:58:08 +0530

    add time slot modules

--- models/master_time_slot.py
+++ models/master_time_slot.py
@@ -1,8 +1,13 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
 from core.database import Base
 
 class MasterTimeSlot(Base):
     __tablename__ = "master_time_slot"
 
     id = Column(Integer, primary_key=True, index=True)
-    time_slot_name = Column(String(255), nullable=False)
+    time_slot = Column(String(255), nullable=False)
+    is_active = Column(Boolean, default=True)
+
+    # relation to home service
+    home_services = relationship("HomeService", backref="time_slot_detail")
