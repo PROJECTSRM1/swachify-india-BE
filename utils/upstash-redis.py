bf74c1218108b3c5a0233bd9205bf431086d80c7
RajashekarRM1 <rajashekar.maddi@rm1codershub.com>
2025-12-17 11:57:57 +0530

    home serivce changed

---
+++ utils/upstash-redis.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+UPSTASH_URL = os.getenv("UPSTASH_REDIS_REST_URL")
+UPSTASH_TOKEN = os.getenv("UPSTASH_REDIS_REST_TOKEN")
+
+HEADERS = {
+    "Authorization": f"Bearer {UPSTASH_TOKEN}"
+}
+
+def redis_setex(key: str, value: str, ttl_seconds: int):
+    url = f"{UPSTASH_URL}/set/{key}/{value}?ex={ttl_seconds}"
+    res = requests.post(url, headers=HEADERS)
+    res.raise_for_status()
+
+def redis_get(key: str):
+    url = f"{UPSTASH_URL}/get/{key}"
+    res = requests.get(url, headers=HEADERS)
+    res.raise_for_status()
+    return res.json().get("result")
+
+def redis_delete(key: str):
+    url = f"{UPSTASH_URL}/del/{key}"
+    res = requests.post(url, headers=HEADERS)
+    res.raise_for_status()
