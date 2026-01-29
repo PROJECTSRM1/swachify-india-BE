53c7086b78e82269dba7e27a58669b2f08244edb
RajashekarRM1 <rajashekar.maddi@rm1codershub.com>
2026-01-29 14:51:16 +0530

    removed models

--- models/master/__init__.py
+++
@@ -1,4 +0,0 @@
-from .master_state import MasterState
-from .master_district import MasterDistrict
-from .master_gender import MasterGender
-from .master_skill import MasterSkill
--- models/master_module.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Boolean
-from core.database import Base
-
-class MasterModule(Base):
-    __tablename__ = "master_module"
-
-    id = Column(Integer, primary_key=True)
-    module_name = Column(String)
-    is_active = Column(Boolean)
