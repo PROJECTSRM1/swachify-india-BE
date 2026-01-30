3a8b831f350293424c0639cf95e32bb5ffd23a3d
RajashekarRM1 <rajashekar.maddi@rm1codershub.com>
2026-01-30 16:27:06 +0530

    asdadf

--- schemas/institution_schema.py
+++ schemas/institution_schema.py
@@ -25,21 +25,26 @@ class InstitutionRegistrationCreate(BaseModel):
     created_by: Optional[int] = None
     is_active: Optional[bool] = True
 
-
 class InstitutionRegistrationResponse(BaseModel):
     id: int
+
     institution_name: str
     institution_type_id: int
     identity_type_id: int
     identity_number: str
     location: str
     representative_name: str
-    email: str
+    email: EmailStr
     phone_number: str
-    institute_website: Optional[str]
-    total_branches: Optional[int]
-    academic_year_start: Optional[date]
-    academic_year_end: Optional[date]
+
+    upload_id_proof: Optional[str] = None
+    upload_address_proof: Optional[str] = None
+    institute_website: Optional[str] = None
+    total_branches: Optional[int] = None
+    academic_year_start: Optional[date] = None
+    academic_year_end: Optional[date] = None
+
+    created_by: Optional[int] = None
     created_date: datetime
     is_active: bool
 
