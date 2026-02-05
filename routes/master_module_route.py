99c7bd144498b105df7f106cd1a6ef2876ab8f29
abhilash <abhi798917@gmail.com>
2026-02-05 10:59:06 +0530

    adeded

--- routes/master_module_route.py
+++ routes/master_module_route.py
@@ -1,29 +1,57 @@
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
+from typing import List, Optional
 
 from core.database import get_db
+from models.generated_models import HomeServiceBooking
+from schemas.home_schema import (
+    HomeServiceBookingCreateSchema,
+    HomeServiceBookingResponseSchema,
+    MasterMechanicCreateSchema,
+    MasterMechanicResponseSchema
+)
 from services.master_module_service import (
-    get_home_service_booking_summary
+    create_home_service_booking,
+    create_master_mechanic,
+    get_home_service_bookings
 )
 
 router = APIRouter(
-    prefix="/master-module",
-    tags=["Master Module"]
+    prefix="/home-service/bookings",
+    tags=["Home Service Booking"]
 )
 
-# ======================================================
-# HOME SERVICE BOOKING SUMMARY
-# ======================================================
 
-@router.get("/home-service-booking-summary")
-def home_service_booking_summary_api(
-    institution_id: int = Query(
-        -1,
-        description="Pass institution_id or -1 to fetch all"
-    ),
+@router.post("",response_model=HomeServiceBookingResponseSchema,status_code=201)
+def create_booking(payload: HomeServiceBookingCreateSchema,db: Session = Depends(get_db)):
+    booking = HomeServiceBooking(**payload.dict(),is_active=True)
+    db.add(booking)
+    db.commit()
+    db.refresh(booking)
+    return booking
+
+
+@router.get(
+    "",
+    response_model=List[HomeServiceBookingResponseSchema]
+)
+def fetch_bookings(
+    id: Optional[int] = Query(None, description="Fetch booking by id"),
     db: Session = Depends(get_db)
 ):
-    return get_home_service_booking_summary(
+    return get_home_service_bookings(
         db=db,
-        institution_id=institution_id
+        booking_id=id
     )
+
+
+@router.post(
+    "/mechanics",
+    response_model=MasterMechanicResponseSchema,
+    status_code=201
+)
+def create_mechanic(
+    payload: MasterMechanicCreateSchema,
+    db: Session = Depends(get_db)
+):
+    return create_master_mechanic(db, payload)
